6aaa463e6944495819c21a7a96a388f93ed1938a
Scott Moeller <electronjoe@gmail.com>
2019-04-02 06:38:42 -0700

    BoundPair: Immutable getters for left and right bound

--- src/bound_pair.rs
+++ src/bound_pair.rs
@@ -14,17 +14,16 @@ where
     T: Copy,
     T: PartialOrd,
 {
-    /// Create a new Bound Pair with lower and upper bounds. If the bounds
-    /// are mal-formed return None.
+    /// Create a new Bound Pair with lower and upper bounds.
+    ///
+    /// If the bounds are mal-formed, i.e. !(left < right), return None.
     ///
     /// # Examples
     ///
     /// ```
     /// use intervals_general::bound_pair::BoundPair;
-    /// use intervals_general::interval::Interval;
     /// # fn main() -> std::result::Result<(), String> {
     /// let bounds = BoundPair::new(1.0, 2.0).ok_or("invalid BoundPair")?;
-    /// let right_half_open = Interval::RightHalfOpen { bound_pair: bounds }; // [1.0, 2.0)
     /// # Ok(())
     /// # }
     /// ```
@@ -42,5 +41,37 @@ where
         } else {
             Some(BoundPair { left, right })
         }
+    }
+
+    /// Fetch an immutable reference to the left bound
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use intervals_general::bound_pair::BoundPair;
+    /// # fn main() -> std::result::Result<(), String> {
+    /// let bounds = BoundPair::new(1.0, 2.0).ok_or("invalid BoundPair")?;
+    /// assert_eq!(*bounds.left(), 1.0);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn left(&self) -> &T {
+        &self.left
+    }
+
+    /// Fetch an immutable reference to the right bound
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use intervals_general::bound_pair::BoundPair;
+    /// # fn main() -> std::result::Result<(), String> {
+    /// let bounds = BoundPair::new(1.0, 2.0).ok_or("invalid BoundPair")?;
+    /// assert_eq!(*bounds.right(), 2.0);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn right(&self) -> &T {
+        &self.right
     }
 }
